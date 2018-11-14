f01b06d0a4c71120829ccc635259f72fb9913ec2
Stewart Kerr <stewarthkerr@gmail.com>
2018-11-13 22:45:53 -0600

    added placeholder comments

--- scripts/getdata.sh
+++ scripts/getdata.sh
@@ -5,7 +5,10 @@ cd "$(dirname "$0")"/..
 #Creates data directory, -p means it won't throw an error if /data already exists
 mkdir -p data
 
-#Pull list of variants from 1001 genome, save to data/quality_variant_list.txt
+#Step 2: Download reference genome
+#Doing this before Step 1 below because it's quicker
+
+#Pull list of variants from 1001 genomes, save to data/quality_variant_list.txt
 wget -O - http://signal.salk.edu/atg1001/download.php |
   grep -oE "id=[^>]*" | cut -d = -f 2 > data/quality_variant_list.txt
 
@@ -14,3 +17,5 @@ while read vname
   do
     wget -O data/quality_variant_$vname.txt http://signal.salk.edu/atg1001/data/Salk/quality_variant_$vname.txt
   done < data/quality_variant_list.txt
+
+#MAYBE: Add a check to make sure all files were downloaded
