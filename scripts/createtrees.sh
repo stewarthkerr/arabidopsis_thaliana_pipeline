4290555160d9ccbf8e6718b73826e1423eaf70b5
Stewart Kerr <stewarthkerr@gmail.com>
2018-11-16 12:46:08 -0600

    updated createtrees.sh to accept 4th argument

--- scripts/createtrees.sh
+++ scripts/createtrees.sh
@@ -2,15 +2,15 @@
 #Use dirname to set the wd to where the script is located, and then go up a level
 cd "$(dirname "$0")"/..
 
-#Takes 3 arguments
+#Takes 4 arguments (3 required, 4th optional)
 # 1: Chromosome (1-5, C, or M)
 # 2: Starting block
 # 3: Number of blocks to produce
-# Files from 4 can be fed directly into this script***
+# 4: Optional flag: -c = all trees for given chromosome, -a = all trees for all chromosomes
 
 #Check to make sure 3 arguments were supplied
 #If time: check to make sure 2 and 3 are VALID integers
-if [ "$#" -ne 3 ]; then
+if [ "$#" -lt 3 ]; then
   echo "create_trees.sh: 3 arguments must be provided"
 elif [[ !("$1" =~ ^(1|2|3|4|5|C|M)$) ]]; then
   echo "create_trees.sh: argument 1 must be one of 12345, C or M"
@@ -19,3 +19,21 @@ elif [[ !($2 =~ ^[0-9]+$) ]]; then
 elif [[ !($3 =~ ^[0-9]+$) ]]; then
   echo "create_trees.sh: argument 3 must be an integer"
 fi
+
+if  [ $4 == '-c']; then
+  for alignment_fname in alignments/chr$1*; do
+    #iqtree -s $alignment_fname --no-outfiles -djc -m HKY+G -nt AUTO -pre ../iqtree
+elif [ $4 == 'c'] then
+  for alignment_fname in alignments/*; do
+    #iqtree -s $alignment_fname --no-outfiles -djc -m HKY+G -nt AUTO -pre ../iqtree
+
+#Calculate starting position - used to determine which file to use
+((sp=$2*10000))
+
+#Calculate ending position -for file name
+((ep=$3*10000))
+
+#echo ../alignments/chr${1}_${sp}_to_${ep}.phy
+
+#Call iqtree for the specified alignments
+#iqtree -s ../alignments/chr$1_(($2*10000))_to_(($3*10000)).phy --no-outfiles -djc -m HKY+G -nt AUTO -pre iqtree/chr$1
