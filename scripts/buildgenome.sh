c4b2ebafeb5b27389f71e27422bb0aa32f7eea28
Stewart Kerr <stewarthkerr@gmail.com>
2018-11-18 16:14:07 -0600

    added argument checks for scripts

---
+++ scripts/buildgenome.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Use dirname to set the wd to where the script is located, and then go up a level
+cd "$(dirname "$0")"/..
+
+#Takes 4 arguments (3 required, 4th optional)
+# 1: Chromosome (1-5, C, or M)
+# 2: Starting base position
+# 3: Ending base position
+
+#Check to make sure 3 arguments were supplied
+#If time: check to make sure 2 and 3 are VALID integers
+if [ "$#" -lt 3 ]; then
+  echo "create_trees.sh: 3 arguments must be provided"; exit 1
+elif [[ !("$1" =~ ^(1|2|3|4|5|C|M)$) ]]; then
+  echo "create_trees.sh: argument 1 must be one of 12345, C or M"; exit 1
+elif [[ !($2 =~ ^[0-9]+$) ]]; then
+  echo "create_trees.sh: argument 2 must be an integer"; exit 1
+elif [[ !($3 =~ ^[0-9]+$) ]]; then
+  echo "create_trees.sh: argument 3 must be an integer"; exit 1
+elif [[ !($2 -gt 0 && $3 -gt 0)]]; then
+  echo "Arguments 2 and 3 must both be greater than zero"; exit 1
+elif [[ ($2 -gt $3) ]]; then
+  echo "Starting base pair must be before ending base pair."; exit 1
+fi
+
+#Short check on genome length
+#Maybe make ending position take last position of chromosome
+bp=$(wc -c data/TAIR10_chr$1.fas | cut -f1 -d' '); echo $bp
+if [[ ($2 -gt $bp) ]]; then
+  echo "chr$1 does not have that many base pairs. Reduce starting position."; exit 1
+elif [[ ($3 -gt $bp) ]]; then
+  echo "chr$1 does not have that many base pairs. Reduce ending position"; exit 1
+fi
+
+#pad start and end position to 8 digits
+sp=$(printf %08d $2)
+ep=$(printf %08d $3)
+
+#Create the .phy file which will hold the genome -- pad to 8 digits
+#Note that if the file already exists, then this will delete the old file
+truncate -s 0 alignments/chr${1}_${sp}_to_${ep}.phy
--- scripts/createtrees.sh
+++ scripts/createtrees.sh
@@ -11,29 +11,32 @@ cd "$(dirname "$0")"/..
 #Check to make sure 3 arguments were supplied
 #If time: check to make sure 2 and 3 are VALID integers
 if [ "$#" -lt 3 ]; then
-  echo "create_trees.sh: 3 arguments must be provided"
+  echo "create_trees.sh: 3 arguments must be provided"; exit 1
 elif [[ !("$1" =~ ^(1|2|3|4|5|C|M)$) ]]; then
-  echo "create_trees.sh: argument 1 must be one of 12345, C or M"
+  echo "create_trees.sh: argument 1 must be one of 12345, C or M"; exit 1
 elif [[ !($2 =~ ^[0-9]+$) ]]; then
-  echo "create_trees.sh: argument 2 must be an integer"
+  echo "create_trees.sh: argument 2 must be an integer"; exit 1
 elif [[ !($3 =~ ^[0-9]+$) ]]; then
-  echo "create_trees.sh: argument 3 must be an integer"
+  echo "create_trees.sh: argument 3 must be an integer"; exit 1
+elif [[ !($2 -gt 0 && $3 -gt 0)]]; then
+  echo "Arguments 2 and 3 must both be greater than zero"; exit 1
 fi
 
-if  [ $4 == '-c']; then
-  for alignment_fname in alignments/chr$1*; do
+#Handle argument 4#
+#if  [ $4 == '-c']; then
+#  for alignment_fname in alignments/chr$1*; do
     #iqtree -s $alignment_fname --no-outfiles -djc -m HKY+G -nt AUTO -pre ../iqtree
-elif [ $4 == 'c'] then
-  for alignment_fname in alignments/*; do
+#elif [ $4 == '-a']; then
+#  for alignment_fname in alignments/*; do
     #iqtree -s $alignment_fname --no-outfiles -djc -m HKY+G -nt AUTO -pre ../iqtree
 
 #Calculate starting position - used to determine which file to use
-((sp=$2*10000))
+((sp=($2-1)*10000+1))
 
 #Calculate ending position -for file name
 ((ep=$3*10000))
 
-#echo ../alignments/chr${1}_${sp}_to_${ep}.phy
+echo ../alignments/chr${1}_${sp}_to_${ep}.phy
 
 #Call iqtree for the specified alignments
 #iqtree -s ../alignments/chr$1_(($2*10000))_to_(($3*10000)).phy --no-outfiles -djc -m HKY+G -nt AUTO -pre iqtree/chr$1
