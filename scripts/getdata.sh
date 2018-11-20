3dce6cb7e35f7722c8749ff935d0bce0e78d967d
Stewart Kerr <stewarthkerr@gmail.com>
2018-11-20 12:35:28 -0600

    download to specific subdirectories

--- scripts/getdata.sh
+++ scripts/getdata.sh
@@ -4,10 +4,12 @@ cd "$(dirname "$0")"/..
 
 #Creates data directory, -p means it won't throw an error if /data already exists
 mkdir -p data
+mkdir -p data/reference
+mkdir -p data/quality_variant
 
 #Step 2: Download reference genome
 #-P lets us specify download directory, -N says don't download if file already downloaded
-wget -N --timestamping -P data/ ftp://ftp.arabidopsis.org/home/tair/Sequences/whole_chromosomes/*.fas
+wget -N --timestamping -P data/reference ftp://ftp.arabidopsis.org/home/tair/Sequences/whole_chromosomes/*.fas
 
 #Pull list of variants from 1001 genomes, save to data/quality_variant_list.txt
 wget -O - http://signal.salk.edu/atg1001/download.php |
@@ -16,7 +18,7 @@ wget -O - http://signal.salk.edu/atg1001/download.php |
 #Pulls SNP data from 1001 genome
 while read vname
   do
-    wget -O data/quality_variant_$vname.txt http://signal.salk.edu/atg1001/data/Salk/quality_variant_$vname.txt
+    wget -N --timestamping -P data/quality_variant http://signal.salk.edu/atg1001/data/Salk/quality_variant_$vname.txt
   done < data/quality_variant_list.txt
 
 #MAYBE: Add a check to make sure all files were downloaded
