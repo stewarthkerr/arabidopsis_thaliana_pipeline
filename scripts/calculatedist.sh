525df26111055231b705413ad7ec2bd73313c038
Youmi Suk <ysuk@wisc.edu>
2018-12-02 21:36:17 -0600

    cleaned version of calculatedist.sh

--- scripts/calculatedist.sh
+++ scripts/calculatedist.sh
@@ -5,36 +5,32 @@ cd "$(dirname "$0")"/..
 # :: step6 calculate distances between all pairs of trees
 # a. between all pairs of trees from the same chromosome, for each chromosome
 # b. between all pairs of trees from consecutive blocks
-# ::::: (which could be extracted from the larger set of distance values above)
 
-mkdir -p treedist/allblocks
+# create folders to save results of iqtree (for a. and b. respectively).
+mkdir -p treedist/allblocks  
 mkdir -p treedist/consecutiveblocks
 
+# Take 1 argument
+# 1: the 3 samllest chromosomes only (2, C, or M)
 
-# create all pairs of trees for a and b.
+# check to make sure 1 arguments was supplied
+if [ "$#" -lt 1 ]; then
+  echo "calculatedist.sh: 1 arguments must be provided"; exit 1
+elif [[ !("$1" =~ ^(2|C|M)$) ]]; then
+  echo "calculatedist.sh: argument 1 must be one of 2, C or M"; exit 1
+fi
 
-for eachtree in iqtree/chrM/chrM*
-    do cat $eachtree >> treedist/allblocks/chrM-all.tre
+
+# create fils with all trees for a and b, respectively. The output is the same but with different names.
+for eachtree in iqtree/chr${1}/chr${1}*
+    do 
+       cat $eachtree >> treedist/allblocks/chr${1}-all.tre
+       cat $eachtree >> treedist/consecutiveblocks/chr${1}-cnsc.tre
     done
 
-for eachtree in iqtree/chrC/chrC*
-    do cat $eachtree >> treedist/allblocks/chrC-all.tre
-    done
 
-for eachtree in iqtree/chrM/chrM*
-    do cat $eachtree >> treedist/consecutiveblocks/chrM-all.tre
-    done
+# calculate distances between all pairs of trees, for a.
+iqtree -t treedist/allblocks/chr${1}-all.tre -rf_all -nt AUTO
 
-for eachtree in iqtree/chrC/chrC*
-    do cat $eachtree >> treedist/consecutiveblocks/chrC-all.tre
-    done
-
-
-# calculate distances between all pairs of trees
-# for a
-iqtree -t treedist/allblocks/chrM-all.tre -rf_all -nt AUTO
-iqtree -t treedist/allblocks/chrC-all.tre -rf_all -nt AUTO
-
-# for b
-iqtree -t treedist/consecutiveblocks/chrM-all.tre -rf_adj -nt AUTO --no-outfiles -quiet
-iqtree -t treedist/consecutiveblocks/chrC-all.tre -rf_adj -nt AUTO --no-outfiles -quiet
+# calculate distances between trees from consecutive blocks, for b.
+iqtree -t treedist/consecutiveblocks/chr${1}-cnsc.tre -rf_adj -nt AUTO --no-outfiles -quiet
